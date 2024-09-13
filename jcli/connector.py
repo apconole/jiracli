apconole/jiracli

commit a9b7a63e09962ecdf72a353afcaf1fcf5cf45cb9

issues: Add support for editing comments.
This includes setting up things like visibility, changing body text and
removing a comment completely.

TBD: Still need the display the visibility when doing a 'show'

Signed-off-by: Aaron Conole <aconole@redhat.com>

jcli/connector.py
@@ -168,14 +168,24 @@ class JiraConnector(object):
 
         return issue_url
 
-    def add_comment(self, issue_identifier, comment_body):
+    def add_comment(self, issue_identifier, comment_body, visibility):
         if self.jira is None:
             raise RuntimeError("Need to log-in first.")
 
         issue = self.get_issue(issue_identifier)
 
+        if isinstance(visibility, str) and visibility != 'all':
+            visibility = {'type': 'group', 'value': visibility}
+
         if issue is not None:
-            self.jira.add_comment(issue, comment_body)
+            self.jira.add_comment(issue, comment_body, visibility)
+
+    def get_comment(self, issue_identifier, comment_id):
+        if self.jira is None:
+            raise RuntimeError("Need to log-in first.")
+
+        comment = self.jira.comment(issue_identifier, comment_id)
+        return comment
 
     def add_watcher(self, issue_identifier, watcher):
         if self.jira is None:

jcli/issues.py
@@ -287,7 +287,9 @@ def show_cmd(issuekey, raw, width):
 @click.argument('issuekey')
 @click.option("--comment", type=str, default=None,
               help="The comment text to add.  Defaults to opening an editor.")
-def add_comment_cmd(issuekey, comment):
+@click.option("--visibility", type=str, default='all',
+              help="Sets the group / role for visibility.  Defaults to 'all'.")
+def add_comment_cmd(issuekey, comment, visibility):
     jobj = connector.JiraConnector()
     jobj.login()
 
@@ -298,7 +300,73 @@ def add_comment_cmd(issuekey, comment):
         click.echo("Error: No comment provided.")
         sys.exit(1)
 
-    jobj.add_comment(issuekey, comment)
+    jobj.add_comment(issuekey, comment, visibility)
+
+
+@click.command(
+    name="del-comment"
+)
+@click.argument('issuekey')
+@click.argument('comment_id')
+def del_comment_cmd(issuekey, comment_id):
+    jobj = connector.JiraConnector()
+    jobj.login()
+
+    issue = jobj.get_issue(issuekey)
+    if issue is None:
+        click.echo(f"Issue {issuekey} not found.")
+        return
+
+    comment = jobj.get_comment(issue.raw['key'], comment_id)
+    if comment is not None:
+        comment.delete()
+        click.echo(f"Comment {comment_id} deleted.")
+    else:
+        click.echo(f"Comment {comment_id} for issue {issuekey} not found.")
+
+
+@click.command('update-comment')
+@click.argument('issuekey')
+@click.argument('comment_id')
+@click.option("--body", type=str, default=None,
+              help="Set new body to the argument.")
+@click.option('--visibility', type=str, default=None,
+              help="Sets the group / role for visibility.  Use 'all' for no restriction.")
+def update_comment_cmd(issuekey, comment_id, body, visibility):
+    jobj = connector.JiraConnector()
+    jobj.login()
+
+    issue = jobj.get_issue(issuekey)
+    if issue is None:
+        click.echo(f"Issue {issuekey} not found.")
+        return
+
+    comment = jobj.get_comment(issue.raw['key'], comment_id)
+    if comment is None:
+        click.echo(f"Comment {comment_id} for issue {issuekey} not found.")
+
+    update = {}
+    body_text = None
+    if body is None:
+        body_text = get_text_via_editor(comment.body)
+    elif body != "":
+        body_text = body
+
+    if body_text:
+        update['body'] = body_text
+
+    if visibility is not None:
+        if visibility.lower() == 'all':
+            update['visibility'] = {'identifier': None}
+        else:
+            update['visibility'] = {'type': 'group', 'value': visibility}
+
+    if len(update) == 0:
+        click.echo("No Changes.")
+        return
+
+    comment.update(**update)
+    click.echo(f"Comment {comment_id} updated.")
 
 
 @click.command(

jcli/shell.py
@@ -76,6 +76,8 @@ issues.add_command(issues_cmds.add_watcher_cmd)
 issues.add_command(issues_cmds.del_watcher_cmd)
 issues.add_command(issues_cmds.attachments_cmd)
 issues.add_command(issues_cmds.get_field_cmd)
+issues.add_command(issues_cmds.del_comment_cmd)
+issues.add_command(issues_cmds.update_comment_cmd)
 
 details.add_command(details_cmds.last_states_cmd)
 details.add_command(details_cmds.server_info_cmd)

commit 568076ad6d30b400d6d852837044b1e0d68b3bb6

connector: Fix comment addition.
The visibility tag is in a bad form when left to default.

Fixes: a9b7a63e09 ("issues: Add support for editing comments.")
Signed-off-by: Aaron Conole <aconole@redhat.com>

jcli/connector.py
@@ -176,6 +176,8 @@ class JiraConnector(object):
 
         if isinstance(visibility, str) and visibility != 'all':
             visibility = {'type': 'group', 'value': visibility}
+        elif isinstance(visibility, str):
+            visibility = None
 
         if issue is not None:
             self.jira.add_comment(issue, comment_body, visibility)