apconole/jiracli

commit 30cd1b85e1cbde41e000bf61979217eaa074c4bf

utils: Add a runtime choice evaluator class.
Click's default choice will populate a choices list at
initial instantiation time.  However, we'd prefer to
delay instantiation until the choices are needed for
choices that go to the server.  Implement a runtime
eval choice class.

Suggested-by: Marcelo Leitner <mleitner@redhat.com>
Signed-off-by: Aaron Conole <aconole@redhat.com>

jcli/utils.py
@@ -282,3 +282,35 @@ def convert_cmd(input, to):
         click.echo(jira_to_md(data))
     else:
         raise RuntimeError(f"Bad conversion {to}")
+
+
+class RuntimeEvalChoice(click.Choice):
+    def __init__(self, choices_getter, **kwargs):
+        self._choice_get = choices_getter
+        self._requested = False
+        super().__init__([], **kwargs)
+
+    def ensure_requested(self):
+        if not self._requested:
+            self.choices = self._choice_get()
+            self._requested = True
+
+    def convert(self, **kwargs):
+        self.ensure_requested()
+        return super().convert(**kwargs)
+
+    def shell_complete(self, **kwargs):
+        self.ensure_requested()
+        return super().shell_complete(**kwargs)
+
+    def __repr__(self):
+        self.ensure_requested()
+        return f"RuntimeEvalChoice({list(self.choices)})"
+
+    def get_metavar(self, self2, **kwargs):
+        self.ensure_requested()
+        choices_str = "|".join(
+            [str(i) for i in self.choices]
+        )
+
+        return f"[{choices_str}]"

commit 5beac5192781102ae83878c82e464bbc120ff3d5

utils: Match the 8.xx series for runtime choices.
Click's definition of choice can change with different versions, but
we should use the most common definition.  The original implementation
tried to use kwargs as a generic standin, but it turns out that doesn't
work properly in Python.

Fixes: 30cd1b85e1cb ("utils: Add a runtime choice evaluator class.")
Signed-off-by: Aaron Conole <aconole@redhat.com>

jcli/utils.py
@@ -295,19 +295,19 @@ class RuntimeEvalChoice(click.Choice):
             self.choices = self._choice_get()
             self._requested = True
 
-    def convert(self, **kwargs):
+    def convert(self, value, param, ctx):
         self.ensure_requested()
-        return super().convert(**kwargs)
+        return super().convert(value, param, ctx)
 
-    def shell_complete(self, **kwargs):
+    def shell_complete(self, ctx, param, incomplete):
         self.ensure_requested()
-        return super().shell_complete(**kwargs)
+        return super().shell_complete(ctx, param, incomplete)
 
     def __repr__(self):
         self.ensure_requested()
         return f"RuntimeEvalChoice({list(self.choices)})"
 
-    def get_metavar(self, self2, **kwargs):
+    def get_metavar(self, param):
         self.ensure_requested()
         choices_str = "|".join(
             [str(i) for i in self.choices]