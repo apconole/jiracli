apconole/jiracli

commit 5beac5192781102ae83878c82e464bbc120ff3d5

utils: Match the 8.xx series for runtime choices.
Click's definition of choice can change with different versions, but
we should use the most common definition.  The original implementation
tried to use kwargs as a generic standin, but it turns out that doesn't
work properly in Python.

Fixes: 30cd1b85e1cb ("utils: Add a runtime choice evaluator class.")
Signed-off-by: Aaron Conole <aconole@redhat.com>

jcli/utils.py
@@ -295,19 +295,19 @@ class RuntimeEvalChoice(click.Choice):
             self.choices = self._choice_get()
             self._requested = True
 
-    def convert(self, **kwargs):
+    def convert(self, value, param, ctx):
         self.ensure_requested()
-        return super().convert(**kwargs)
+        return super().convert(value, param, ctx)
 
-    def shell_complete(self, **kwargs):
+    def shell_complete(self, ctx, param, incomplete):
         self.ensure_requested()
-        return super().shell_complete(**kwargs)
+        return super().shell_complete(ctx, param, incomplete)
 
     def __repr__(self):
         self.ensure_requested()
         return f"RuntimeEvalChoice({list(self.choices)})"
 
-    def get_metavar(self, self2, **kwargs):
+    def get_metavar(self, param):
         self.ensure_requested()
         choices_str = "|".join(
             [str(i) for i in self.choices]

commit 8783f1c0ff8417848bba89b53c5f13b3e44260cd

utils: Missed a hunk with RuntimeEvalChoice.
When fixing the definitions as part of commit 5beac5192781
("utils: Match the 8.xx series for runtime choices"), the context
details for the get_metavar used building choices details was
omitted.

Fixes: 5beac5192781 ("utils: Match the 8.xx series for runtime choices")
Signed-off-by: Aaron Conole <aconole@redhat.com>

jcli/utils.py
@@ -313,7 +313,7 @@ class RuntimeEvalChoice(click.Choice):
         self.ensure_requested()
         return f"RuntimeEvalChoice({list(self.choices)})"
 
-    def get_metavar(self, param):
+    def get_metavar(self, param, ctx):
         self.ensure_requested()
         choices_str = "|".join(
             [str(i) for i in self.choices]