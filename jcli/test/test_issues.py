apconole/jiracli

commit f0988025f6a6b660254f4f0e8a30673a1e64a5ad

issues: Allow issue creation to pass the forced flag.
When setting issue fields during issue creation it can be
important to set the field value using the forced flag.

Signed-off-by: Aaron Conole <aconole@redhat.com>

jcli/issues.py
@@ -838,11 +838,11 @@ def create_issue_cmd(ctx, summary, description, project, issue_type, set_field,
 
     for c in comments:
         if c.startswith("# set-field:"):
-            m = re.match(r'# set-field: "(.*)" (.*)', c)
+            m = re.match(r'# set-field:\s*(--forced\s+)"(.*)" (.*)', c)
             if m:
-                f, v = m.groups()
+                forced, f, v = m.groups()
                 f = jobj._try_fieldname(f)
-                v = jobj.convert_to_field_type(f, v)
+                v = jobj.convert_to_field_type(f, v) if not forced else eval(v)
                 issue[f] = v
         elif c.startswith("# set-project: "):
             project = c[15:]

commit cf329f57e2d13ceb2853b3f22a3bda8b530223d4

issues: Fix the issue creation regex with field setters.
During f0988025f6a6 ("issues: Allow issue creation to pass the forced flag.")
the regex was modified to allow passing the forced flag.  Unfortunately, that
caused the field matching code to require --forced to be included due to the
regex missing a crucial '?' to allow for 0-1 instances of --forced to exist.

Fixes: f0988025f6a6 ("issues: Allow issue creation to pass the forced flag.")
Reported-by: Chris van Hoof <vanhoof@ouwish.com>
Reported-at: https://github.com/apconole/jiracli/issues/17
Signed-off-by: Aaron Conole <aconole@redhat.com>

jcli/issues.py
@@ -908,6 +908,7 @@ def create_issue_cmd(ctx, summary, description, project, issue_type, set_field,
             f = '"' + f + '"'
             fields[f] = "value"
 
+    special_lines += "#\n"
     for f in set_field:
         field = f[0]
         if not field.startswith('"'):
@@ -936,7 +937,7 @@ def create_issue_cmd(ctx, summary, description, project, issue_type, set_field,
 
     for c in comments:
         if c.startswith("# set-field:"):
-            m = re.match(r'# set-field:\s*(--forced\s+)"(.*)" (.*)', c)
+            m = re.match(r'# set-field:\s*(--forced)?\s*"(.*)" (.*)', c)
             if m:
                 forced, f, v = m.groups()
                 f = jobj._try_fieldname(f)

jcli/test/stubs.py
@@ -64,6 +64,7 @@ class JiraAuthorStub(dict):
 class JiraConnectorStub(JiraConnector):
     _issues_list = []
     _last_jql = ""
+    last_issue = None
     config = {}
 
     def reset_config(cfg=None):
@@ -185,6 +186,7 @@ class JiraConnectorStub(JiraConnector):
            'issuetype' not in issue_dict or \
            'project' not in issue_dict:
             raise ValueError("Missing required elements")
+        JiraConnectorStub.last_issue = issue_dict
         return 'ISSUE-abc'
 
     def add_comment(self, issue_identifier, comment_body, visibility):

jcli/test/test_issues.py
@@ -5,6 +5,7 @@ from jcli.issues import create_issue_cmd
 from jcli.issues import get_field_cmd
 from jcli.test.stubs import JiraConnectorStub
 import json
+import pprint
 import pytest
 import random
 import re
@@ -148,7 +149,7 @@ def test_reply_to_cmd(cli_runner):
 
 
 @patch('jcli.connector.JiraConnector', JiraConnectorStub)
-def test_issue_create(cli_runner):
+def test_issue_create(cli_runner, tmpdir):
     JiraConnectorStub.setup_clear_issues()
 
     result = cli_runner.invoke(create_issue_cmd, obj={})
@@ -166,6 +167,45 @@ def test_issue_create(cli_runner):
                                 '--issue-type', 'Bug'], obj={})
     assert result.exit_code == 0
     assert "done - Result: " in result.output
+
+    # Now test with set-commands
+    issue_project = "DEF"
+    result = cli_runner.invoke(create_issue_cmd,
+                               ['--verbose',
+                                '--summary', issue_summary,
+                                '--description', issue_description,
+                                '--project', issue_project,
+                                '--issue-type', 'Bug',
+                                '--set-field', 'priority', 'Medium',
+                                '--set-field', 'duedate', '2025-07-15'], obj={})
+    assert result.exit_code == 0
+    assert JiraConnectorStub.last_issue is not None
+    print(result.output)
+    print(f"Issue: {pprint.pformat(JiraConnectorStub.last_issue)}")
+    assert 'priority' in JiraConnectorStub.last_issue
+    assert 'duedate' in JiraConnectorStub.last_issue
+
+    # Now test 'forced' flag
+    issue_set_text = f"""# Starting an issue
+{issue_summary} - from file.
+
+DESC: {issue_description}
+
+# set-project: {issue_project}
+# issue-type: Bug
+# set-field: --forced "priority" int(1234)
+# set-field: "duedate" 2025-07-15
+"""
+    f = tmpdir.join("issue_txt")
+    f.write(issue_set_text)
+    cli_runner.invoke(create_issue_cmd,
+                      ['--verbose',
+                       '--from-file', str(f)],
+                      obj={})
+    assert result.exit_code == 0
+    assert JiraConnectorStub.last_issue is not None
+    assert 'priority' in JiraConnectorStub.last_issue
+    assert JiraConnectorStub.last_issue['priority'] == 1234
 
 
 @patch('jcli.connector.JiraConnector', JiraConnectorStub)